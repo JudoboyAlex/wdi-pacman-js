7b6d0f26032b6e1cdeb4f7acb75caf97d0defc59
Alexander G. Yang <spark8402@rogers.com>
2019-06-13 20:58:54 -0400

    assignment done

--- pacman.js
+++ pacman.js
@@ -1,11 +1,45 @@
 // Setup initial game stats
 let score = 0;
 let lives = 2;
+let powerPellets = 4;
 
 
 // Define your ghosts here
+const inky = {
+  menu_option: '1',
+  name: 'Inky',
+  colour: 'Red',
+  character: 'Shadow',
+  edible: false
+};
+
+const blinky = {
+  menu_option: '2',
+  name: 'Blinky',
+  colour: 'Cyan',
+  character: 'Speedy',
+  edible: false
+};
+
+const pinky = {
+  menu_option: '3',
+  name: 'Pinky',
+  colour: 'Pink',
+  character: 'Bashful',
+  edible: false
+};
+
+const clyde = {
+  menu_option: '4',
+  name: 'Clyde',
+  colour: 'Orange',
+  character: 'Pokey',
+  edible: false
+};
+
 
 // replace this comment with your four ghosts setup as objects
+ghosts = [inky, blinky, pinky, clyde]
 
 
 // Draw the screen functionality
@@ -24,11 +58,18 @@ function clearScreen() {
 
 function displayStats() {
   console.log(`Score: ${score}     Lives: ${lives}`);
+  console.log(`\nPellets: ${powerPellets}`);
 }
 
+// Use the array of ghosts you setup in the previous step to cycle through and display each ghost.
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  console.log('(p) Eat Power-Pellet');
+
+  for(i=0; i < ghosts.length; i++){
+    console.log(`(${i+1}) Eat ${ghosts[i].name} (${ghosts[i].edible? "Edible": "Not Edible"})`); 
+  }
   console.log('(q) Quit');
 }
 
@@ -44,6 +85,37 @@ function eatDot() {
   score += 10;
 }
 
+// add a function called eatGhost that accepts a ghost as an argument
+// the eatGhost should check to see if a ghost is edible. If it's not, Pac-Man should lose a life
+// include a quick sentence that says the name and colour of the ghost that kills Pac-Man (similar to how it quickly flashes chomp on the screen when you eat a dot)
+
+function eatGhost(ghost){
+  if (ghost.edible == false){
+      lives -= 1;     
+    console.log(`\n${ghost.name} that has ${ghost.colour} colour just killed you bro`)
+  } else if (
+    ghost.edible == true
+    ){
+    console.log(`I just ate ${ghost.name} that are ${ghost.character}.`);
+    score += 200;
+    ghost.edible = false;   
+  }
+}
+
+// Create an eatPowerPellet function that's executed when the p key is entered. It should:
+
+// increase Pac-Man's score by 50 points
+// change all the ghost's edible property to true
+// reduce the number of Power-Pellets remaining
+
+function eatPowerPellet(){
+  for (i=0; i < ghosts.length; i++){
+    ghosts[i].edible = true;
+  }
+  score += 50;
+  powerPellets -= 1;
+}
+
 
 // Process Player's Input
 function processInput(key) {
@@ -55,11 +127,43 @@ function processInput(key) {
     case 'd':
       eatDot();
       break;
+    case 'p':
+      if (powerPellets > 0) {
+         eatPowerPellet();
+         break;
+      } else{
+        console.log('\n no more left power pellets left!');
+        break;
+      }
+       
+    case '1':
+      eatGhost(inky);
+      gameover();
+      break;
+    case '2':
+      eatGhost(blinky);
+      gameover();
+      break;
+    case '3':
+      eatGhost(pinky);
+      gameover();
+      break;
+    case '4':
+      eatGhost(clyde);
+      gameover();
+      break;
     default:
       console.log('\nInvalid Command!');
   }
 }
 
+// If Pac-Man's lives go below 0, it's Game Over and you should exit the game. Create a function that checks for this every time Pac-Man loses a life, and calls process.exit(); if necessary.
+
+function gameover(){
+  if (lives < 0){ 
+    process.exit();
+  }
+}
 
 //
 // YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
@@ -78,10 +182,12 @@ drawScreen();
 stdin.on('data', (key) => {
   process.stdout.write(key);
   processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+  setTimeout(drawScreen, 500); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
 process.on('exit', () => {
+
   console.log('\n\nGame Over!\n');
+  
 });
